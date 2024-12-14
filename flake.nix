653e6bb1bc71cfd1fe51b7b6eae9e3abceae9439
Vele Dan Alexandru <vele.dan.alexandru@gmail.com>
2024-12-14 08:44:48 +0200

    Squash - initial commit

--- flake.nix
+++ flake.nix
@@ -1,15 +1,63 @@
 {
-  description = "A very basic flake";
+  description = "CMakeLists.txt";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        # To import a flake module
+        # 1. Add foo to inputs
+        # 2. Add foo as a parameter to the outputs function
+        # 3. Add here: foo.flakeModule
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+      ];
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
 
-  };
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          # Per-system attributes can be defined here. The self' and inputs'
+          # module parameters provide easy access to attributes of the same
+          # system.
+
+          # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
+          packages.default = pkgs.hello;
+
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              nixfmt-rfc-style
+              cowsay
+              llvmPackages_19.clang-tools
+            ];
+
+            formatter = pkgs.nixfmt-rfc-style;
+          };
+        };
+
+      flake = {
+        # The usual flake attributes can be defined here, including system-
+        # agnostic ones like nixosModule and system-enumerating ones, although
+        # those are more easily expressed in perSystem.
+
+      };
+    };
 }
---
+++ src/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int
+main(
+        [[maybe_unused]] int   argc,
+        [[maybe_unused]] char* argv[argc]
+) {
+        puts("Auxilium C.");
+
+        return 0;
+}
